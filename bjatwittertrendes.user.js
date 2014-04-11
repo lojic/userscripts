711e7e5e2e85f545d297bd6af9f12e58f38ba5d8
Brian Adkins <info@lojic.com>
2014-04-11 13:57:17 -0400

    Userscript to remove trends from twitter web ui

---
+++ bjatwittertrendes.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name           BJATwitterTrends
+// @namespace      http://lojic.com
+// @description    Remove trends from twitter 2
+// @include        http://twitter.com/*
+// @include        https://twitter.com/*
+// ==/UserScript==
+
+var GM_addGlobalStyle = function(css) {
+  var sel=document.createElement('style'); 
+  sel.setAttribute('type','text/css'); 
+  sel.appendChild(document.createTextNode(css));
+  var hel=document.documentElement.firstChild; 
+
+  while(hel && hel.nodeName!='HEAD') { 
+    hel=hel.nextSibling; 
+  }
+  
+  if(hel && hel.nodeName=='HEAD') { 
+    hel.appendChild(sel); 
+  } else { 
+    document.body.insertBefore(sel,document.body.firstChild); 
+  }
+  
+  return sel;
+}
+
+GM_addGlobalStyle('.trends-inner { display: none; }');
